ff88184b6d0eefe9d9a545c040cc78c3292ade1b
mouseleo <653642247@qq.com>
2020-12-30 12:02:23 +0800

    update sso-server-test

--- tests/Feature/SsoServerTest.php
+++ tests/Feature/SsoServerTest.php
@@ -1,3 +1,21 @@
 <?php
 
 namespace Mouseleo\SsoClient\Test;
+
+use Illuminate\Support\Facades\Http;
+use Orchestra\Testbench\TestCase;
+
+class SsoServerTest extends TestCase
+{
+    public function test_can_access_the_sso_server()
+    {
+        $response = Http::get('http://sso.test');
+
+        $this->assertTrue($response->successful());
+    }
+
+    public function test_the_sso_server_api_is_ok()
+    {
+        //
+    }
+}
